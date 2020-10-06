ed2f55cec788368836eb3ec41a4cd08e1bda7c39
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-10-06 09:09:16 -0700

    add card and stack resources

---
+++ backend/db/migrate/20201006154323_create_cards.rb
@@ -0,0 +1,10 @@
+class CreateCards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cards do |t|
+      t.string :front
+      t.string :back
+      t.integer :stack_id
+      t.timestamps
+    end
+  end
+end
