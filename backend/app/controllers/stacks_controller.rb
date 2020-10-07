ea8682bf3b56b8aa323db2866a689efb6e2a0e22
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-10-07 10:56:34 -0700

    add show and create methods

--- backend/app/controllers/stacks_controller.rb
+++ backend/app/controllers/stacks_controller.rb
@@ -3,4 +3,14 @@ class StacksController < ApplicationController
     stacks = Stack.all
     render json: stacks
   end
+
+  def show
+    stack = Stack.find(params[:id])
+    render json: stack.cards
+  end
+  
+  def create
+    stack = Stack.create(title: params[:stack][:title])
+    render json: stack
+  end
 end
