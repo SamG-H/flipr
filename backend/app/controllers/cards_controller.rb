046bdc34412c69885b3dd239e05b7d268420a9c8
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-10-08 13:30:39 -0700

    add create method and strong params

--- backend/app/controllers/cards_controller.rb
+++ backend/app/controllers/cards_controller.rb
@@ -3,4 +3,14 @@ class CardsController < ApplicationController
     card = Card.find(params[:id])
     render json: card, include: :stack
   end
+
+  def create
+    card = Card.create(card_params)
+    render json: card, include: :stack
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:front, :back, :stack_id)
+  end
 end
