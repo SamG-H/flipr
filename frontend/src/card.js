ff2855f97d5f583479fa8f049cc64243977c715c
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-10-09 11:44:04 -0700

    rename id

--- frontend/src/card.js
+++ frontend/src/card.js
@@ -6,25 +6,20 @@ class Card{
     }
 
     display(stack){
-	const cardDiv = document.getElementById(`${stack.id}`);
+	const stackDiv = document.getElementById(`stack-${stack.id}`);
 	const stackBtn = document.getElementById(`${stack.id} btn`);
 	if(stackBtn){
 	    stackBtn.remove();
 	}
 	//stackBtn.innerText = "Hide this stack"; maybe change eventListener on this button instead of removing
+	const cardDiv = document.createElement('div');
+	cardDiv.setAttribute('id', `${this.id}`);
 	const front = document.createElement('p');
 	const back = document.createElement('p');
-	const nextBtn = document.createElement('button');
-	const prevBtn = document.createElement('button');
-	nextBtn.innerText = "->";
-	prevBtn.innerText = "<-";
-	prevBtn.addEventListener("click", (e) => stack.renderPreviousCard);
-	nextBtn.addEventListener("click", (e) => stack.renderNextCard);
 	front.innerText = `front: ${this.front}`;
 	back.innerText = `back: ${this.back}`;
 	cardDiv.appendChild(front);
 	cardDiv.appendChild(back);
-	cardDiv.appendChild(prevBtn);
-	cardDiv.appendChild(nextBtn);
+	stackDiv.appendChild(cardDiv);
     }
 }
