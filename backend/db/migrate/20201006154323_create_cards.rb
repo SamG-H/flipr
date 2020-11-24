53794e6d40f56bb4e2da590a940914a077404512
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-24 14:05:28 -0800

    switch to postgres

--- backend/db/migrate/20201006154323_create_cards.rb
+++ backend/db/migrate/20201006154323_create_cards.rb
@@ -1,8 +1,8 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
-      t.string :front
-      t.string :back
+      t.text :front
+      t.text :back
       t.integer :stack_id
       t.timestamps
     end
--- backend/db/migrate/20201117190548_create_scores.rb
+++ backend/db/migrate/20201117190548_create_scores.rb
@@ -1,7 +1,7 @@
 class CreateScores < ActiveRecord::Migration[6.0]
   def change
     create_table :scores do |t|
-      t.string :name
+      t.text :name
       t.integer :percentage
       t.integer :stack_id
       t.timestamps
